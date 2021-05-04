8a2362c32615fb6e2eab8b7fc51be3268c16d244
PariaEsmatloo <77804192+PariaEsmatloo@users.noreply.github.com>
2021-05-04 12:55:55 -0500

    Create Reacher_REINFORCE_Method.py

---
+++ REINFORCE_Method/Reacher_v2_REINFORCE/Reacher_REINFORCE_Method.py
@@ -0,0 +1,196 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import base64
+import imageio
+import IPython
+import matplotlib.pyplot as plt
+import PIL.Image
+import pyvirtualdisplay
+import tensorflow as tf
+from tf_agents.agents.reinforce import reinforce_agent
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.networks import actor_distribution_network
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+import numpy as np
+import time
+from IPython import get_ipython
+import csv  
+
+tf.compat.v1.enable_v2_behavior()
+env_name = 'Reacher-v2' # @param {type:"string"}
+num_iterations = 10 # @param {type:"integer"}
+collect_episodes_per_iteration = 1 # @param {type:"integer"}
+replay_buffer_capacity = 2000 # @param {type:"integer"}
+fc_layer_params = (100,)
+learning_rate = 1e-3 # @param {type:"number"}
+log_interval = 1 # @param {type:"integer"}
+num_eval_episodes = 1 # @param {type:"integer"}
+eval_interval = 1 # @param {type:"integer"}
+
+
+env = suite_gym.load(env_name)
+env.reset()
+time_step = env.reset()
+
+action = np.array(2, dtype=np.float)
+
+next_time_step = env.step(action)
+
+
+
+train_py_env = suite_gym.load(env_name)
+eval_py_env = suite_gym.load(env_name)
+
+train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+
+actor_net = actor_distribution_network.ActorDistributionNetwork(
+    train_env.observation_spec(),
+    train_env.action_spec(),
+    fc_layer_params=fc_layer_params)
+
+
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+train_step_counter = tf.compat.v2.Variable(0)
+
+tf_agent = reinforce_agent.ReinforceAgent(
+    train_env.time_step_spec(),
+    train_env.action_spec(),
+    actor_network=actor_net,
+    optimizer=optimizer,
+    normalize_returns=False,
+    train_step_counter=train_step_counter)
+tf_agent.initialize()
+
+eval_policy = tf_agent.policy
+collect_policy = tf_agent.collect_policy
+
+def compute_avg_return(environment, policy, num_episodes=1):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+    data_spec=tf_agent.collect_data_spec,
+    batch_size=train_env.batch_size,
+    max_length=replay_buffer_capacity)
+
+
+def collect_episode(environment, policy, num_episodes):
+
+  episode_counter = 0
+  environment.reset()
+
+  while episode_counter < num_episodes:
+    time_step = environment.current_time_step()
+    action_step = policy.action(time_step)
+    next_time_step = environment.step(action_step.action)
+    traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+    # Add trajectory to the replay buffer
+    replay_buffer.add_batch(traj)
+
+    if traj.is_boundary():
+      episode_counter += 1
+
+
+# (Optional) Optimize by wrapping some of the code in a graph using TF function.
+tf_agent.train = common.function(tf_agent.train)
+
+# Reset the train step
+tf_agent.train_step_counter.assign(0)
+
+# Evaluate the agent's policy once before training.
+avg_return = compute_avg_return(eval_env, tf_agent.policy, num_eval_episodes)
+returns = [avg_return]
+
+for _ in range(num_iterations):
+
+  # Collect a few episodes using collect_policy and save to the replay buffer.
+  collect_episode(
+      train_env, tf_agent.collect_policy, collect_episodes_per_iteration)
+
+  # Use data from the buffer and update the agent's network.
+  experience = replay_buffer.gather_all()
+  train_loss = tf_agent.train(experience)
+  replay_buffer.clear()
+
+  step = tf_agent.train_step_counter.numpy()
+
+  if step % log_interval == 0:
+    print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+
+  if step % eval_interval == 0:
+    avg_return = compute_avg_return(eval_env, tf_agent.policy, num_eval_episodes)
+    print('step = {0}: Average Return = {1}'.format(step, avg_return))
+    returns.append(avg_return)
+
+steps = range(0, num_iterations + 1, eval_interval)
+
+# data = []
+# for i in range(2):
+#     data.append([])
+
+# for items in steps: 
+#     data[0].append(items)
+# for item in returns:
+#     data[1].append(item)
+# print(data)
+
+rows = zip(steps,returns)
+
+with open('Reinforce_Trial1.csv', 'w') as f:
+    writer = csv.writer(f,quoting=csv.QUOTE_ALL)
+    for items in rows:
+        writer.writerow(items)
+
+
+
+plt.plot(steps, returns)
+plt.ylabel('Total Return')
+plt.xlabel('Episode')
+plt.ylim(top=200)
+  
+def embed_mp4(filename):
+  """Embeds an mp4 file in the notebook."""
+  video = open(filename,'rb').read()
+  b64 = base64.b64encode(video)
+  tag = '''
+  <video width="640" height="480" controls>
+    <source src="data:video/mp4;base64,{0}" type="video/mp4">
+  Your browser does not support the video tag.
+  </video>'''.format(b64.decode())
+
+  return IPython.display.HTML(tag)
+num_episodes = 20
+video_filename = 'Reacher_reinforce.mp4'
+with imageio.get_writer(video_filename, fps=60) as video:
+  for _ in range(num_episodes):
+    time_step = eval_env.reset()
+    video.append_data(eval_py_env.render())
+    while not time_step.is_last():
+      action_step = tf_agent.policy.action(time_step)
+      time_step = eval_env.step(action_step.action)
+      video.append_data(eval_py_env.render())
+
+embed_mp4(video_filename)
+        
